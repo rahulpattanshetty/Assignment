f85e5ffb6b9036cda504781a04063bc8960abd37
Rahul <rahulpattanshetty@gmail.com>
2017-09-14 22:04:53 +0530

    added stock program

--- stock.rb
+++ stock.rb
@@ -1,89 +1,38 @@
-class Stock
-	attr_accessor :name,:price
-
-	def initialize(details)
-		@name = details[:name]
-		@price = details[:price]
-	end
-	def details
-		puts "#{name}-Price #{price}"
-		
-	end
-
-end
-
-class User
-	attr_accessor :stock_name, :quantity, :price, :status
-
-	def initailize(details)
-		@stock_name = details[:stock_name]
-		@quantity = details[:quantity]
-		@price = details[:price]
-		@status = details[:status]
-	end
-	def details
-		puts "#{stock_name} #{quantity} #{price}"
-		
-	end
-	
-end
-
-s1 = Stock.new({:name =>"RIL", :price =>231})
-s2 = Stock.new({:name =>"BEL", :price =>412})
-stock = []
-stock.push(s1,s2)
-puts "Stocks available are"
-puts "*" * 50
-stock.each do |s|
-		s.details
-	end
-puts "*" * 50
 
 
-u = User.new
 
-count = 0
+input = "ZNGA 1300 2.62 B, CLH15.NYM 50 56 B, OWW 1000 11 B, OGG 20 580.1 B"
 
-user = []
-cont = "y"
-while cont == "y"
-	puts "Enter Your choice buy/sell (b/s)"
-	choice = gets.chomp
-	case choice
-	when "b"
-		puts "Name of the stock"
-		u.stock_name=gets.chomp
-		puts "How many quantity"
-		u.quantity=gets.to_i
-		u.price = s1.price
-		u.status = "B"
-		user.push(u)
-		puts "#{u.stock_name} #{u.quantity} #{u.price} B"
-
-	when "s"
-		puts "*" * 50
-		puts "List of Stocks"
-		puts "*" * 50
-		u.details
-		puts "*" * 50
-		puts "Name of the stock"
-		u.stock_name=gets.chomp
-		puts "How many quantity"
-		u.quantity=gets.to_i
-		puts "At what price"
-		price = gets.to_f.round
-		
-		if price == 0
-			count +=1
-			puts "Buy: #{u.price} Sell: #{price}; Badly formed #{count}: badly-formed 1st simple order ;badly-formed nth simple order ;"
-		else
-			u.price = price
-			puts "#{u.stock_name} #{u.quantity} #{u.price} S"	
-		end
-		
+stock = ""
+quantity = 0
+price = 0
+status = ""
+total_b = 0
+total_s = 0
+badly_formed = 0
+array = input.split(",")
+errors = ""
+array.each do |item|
+	stock = item.split(" ")[0]
+	quantity = item.split(" ")[1]
+	price = item.split(" ")[2]
+	status = item.split(" ")[3]
+	if price.include?(".") && !(quantity.include?("."))
+		if status == "B"
+			total_b = total_b + quantity.to_i * price.to_f 
+		elsif status == "S"
+			total_s = total_s + quantity.to_i * price.to_f
+		end	
 	else
-		puts "Invalid option"
+		badly_formed +=1
+		errors += item + ";"
+			
 	end
-	puts "Do you want cont.(y/n)"
-		cont=gets.chomp
+end
+
+if errors.empty?
+	puts "Buy:#{total_b.round} Sell:#{total_s.round}"
+else
+	puts "Buy:#{total_b.round} Sell:#{total_s.round}; Badly formed #{badly_formed}:#{errors}"
+
 end
